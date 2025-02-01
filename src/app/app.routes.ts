f338b787f04393b84890f7745a2b81f7802d43c1
teoto70 <you@example.com>
2025-02-01 17:07:24 +0200

    Add upload post component with form handling and preview functionality; update routing to include upload path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './user/login/login.component';
 import { CreateUserComponent } from './user/create-user/create-user.component';
 import { MainComponent } from './components/main/main.component';
+import { UploadPostComponent } from './components/upload-post/upload-post.component'; // Adjust the path as needed
+
 
 export const routes: Routes = [
     
     { path: 'home', component: MainComponent},
     {path: 'creator/login', component: LoginComponent},
     {path: 'admin/createUser', component: CreateUserComponent},
-    {path: '**', redirectTo: '/home'}
+    {path: '**', redirectTo: '/home'},
+    { path: '/upload', component: UploadPostComponent},
 ];
